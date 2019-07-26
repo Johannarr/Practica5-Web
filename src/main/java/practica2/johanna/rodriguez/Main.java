5d53869f6f524ad52c7f5841c6c26942ecfa1013
Johanna Rod <39653403+yohannarr@users.noreply.github.com>
2019-07-26 17:09:54 -0400

    Cambiando puerto

--- src/main/java/practica2/johanna/rodriguez/Main.java
+++ src/main/java/practica2/johanna/rodriguez/Main.java
@@ -27,7 +27,7 @@ public class Main {
         if (process.environment().get("PORT") != null) {
             port = Integer.parseInt(process.environment().get("PORT"));
         } else {
-            port = 4567;
+            port = 8080;
         }
 
         setPort(port);
